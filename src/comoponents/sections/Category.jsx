d4fa496ff2b67172f1fe01051bb66dec8c0eb5c7
Shazia906 <shaziatufail5033@gmail.com>
2025-04-30 11:13:52 +0500

    changes

--- src/comoponents/sections/Category.jsx
+++ src/comoponents/sections/Category.jsx
@@ -1,9 +1,9 @@
 
 import React, { useEffect, useState } from "react";
-import Navbar from "../Commons/Nav";
-import Footer from "../Commons/Footer";
-import HomeCard from "../Commons/Cards";
-import Pagination from "../Commons/pagination";
+import Navbar from "../commons/Nav";
+import Footer from "../commons/Footer";
+import HomeCard from "../commons/Cards";
+import Pagination from "../commons/pagination";
 
 const categories = [
   { id: 1, name: "General" },
--- src/comoponents/sections/Home.jsx
+++ src/comoponents/sections/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "../Commons/Nav";
-import Footer from '../Commons/Footer';
-import HomeCard from "../Commons/Cards";
-import Pagination from "../Commons/pagination";
+import Navbar from "../commons/Nav";
+import Footer from '../commons/Footer';
+import HomeCard from "../commons/Cards";
+import Pagination from "../commons/pagination";
 
 const News = [
   { id: 1, name: "Pakistan" },
